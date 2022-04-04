726874e8e71c37d13e621deb2c0aee0caf3ba4d4
ttseel <ojs7928@gmail.com>
2022-04-04 23:26:53 +0900

    codility TennisTournament

---
+++ src/main/java/codility/Exercises/Exercise2/TennisTournament/TennisTournament.java
@@ -0,0 +1,10 @@
+package codility.Exercises.Exercise2.TennisTournament;
+
+public class TennisTournament {
+
+    public int solution(int P, int C) {
+        // write your code in Java SE 8
+
+        return Math.min(P / 2, C);
+    }
+}
